637dad0d33345e60099fcd00a6b4e09a7690b852
Leonkeneddy86 <jonathan19.jtv@gmail.com>
2025-01-31 19:29:13 +0100

    feat:migrations

---
+++ database/migrations/2025_01_31_182524_create_flights_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('flights', function (Blueprint $table) {
+            $table->id();
+            $table->date("date");
+            $table->string("departure");
+            $table->string("arrival");
+            $table->string("image");
+            $table->foreignId("airplane_id")->constrained("planes");
+            $table->boolean("status");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('flights');
+    }
+};
