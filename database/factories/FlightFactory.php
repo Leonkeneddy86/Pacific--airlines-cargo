af9976278cb2f59e877ffc7efe659fb405554964
Leonkeneddy86 <jonathan19.jtv@gmail.com>
2025-01-31 20:57:22 +0100

    feat:factories

---
+++ database/factories/FlightFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+use App\Models\Flight;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Flight>
+ */
+class FlightFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'date' => $this->faker->date(),
+            'departure' => $this->faker->city(),
+            'arrival' => $this->faker->city(),
+            'image' => $this->faker->imageUrl(),
+            'airplane_id' => $this->faker->numberBetween(1, 10),
+            'available' => $this->faker->boolean(),
+        ];
+    }
+}
