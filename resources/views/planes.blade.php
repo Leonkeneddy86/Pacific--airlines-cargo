04e64ccf450c2de29900a8c905fd6df3f38935e8
Leonkeneddy86 <jonathan19.jtv@gmail.com>
2025-02-04 19:37:22 +0100

    feat: view planes

--- resources/views/planes.blade.php
+++ resources/views/planes.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="es">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -11,16 +11,16 @@
         <h1 class="mt-5">Lista de aviones</h1>
         <p>A continuación se muestra una lista de planes disponibles:</p>
         <ul class="list-group">
-            <li class="list-group-item">Destino 1: Vuelo a España</li>
-            <li class="list-group-item">Destino 2: Vuelo a Ecuador</li>
-            <li class="list-group-item">Destino 3: Vuelo a Cuba</li>
-            <li class="list-group-item">Destino 4: Vuelo a Argentina</li>
-            <li class="list-group-item">Destino 5: Vuelo a Colombia</li>
-            <li class="list-group-item">Destino 6: Vuelo a Miami</li>
-            <li class="list-group-item">Destino 7: Vuelo a San Francisco</li>
-            <li class="list-group-item">Destino 8: Vuelo a Africa</li>
-            <li class="list-group-item">Destino 9: Vuelo a Nueva York</li>
-            <li class="list-group-item">Destino 10: Vuelo a Londres</li>
+            <li class="list-group-item">Destiny 1: Vuelo a España</li>
+            <li class="list-group-item">Destiny 2: Vuelo a Ecuador</li>
+            <li class="list-group-item">Destiny 3: Vuelo a Cuba</li>
+            <li class="list-group-item">Destiny 4: Vuelo a Argentina</li>
+            <li class="list-group-item">Destiny 5: Vuelo a Colombia</li>
+            <li class="list-group-item">Destiny 6: Vuelo a Miami</li>
+            <li class="list-group-item">Destiny 7: Vuelo a San Francisco</li>
+            <li class="list-group-item">Destiny 8: Vuelo a Africa</li>
+            <li class="list-group-item">Destiny 9: Vuelo a Nueva York</li>
+            <li class="list-group-item">Destiny 10: Vuelo a Londres</li>
         </ul>
     </div>
 </body>
