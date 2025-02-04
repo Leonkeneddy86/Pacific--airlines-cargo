f183e325a8eea1d818c05c87a4d39b232b22405d
Leonkeneddy86 <jonathan19.jtv@gmail.com>
2025-02-04 15:59:27 +0100

    feat: relations models

--- app/Models/Flight.php
+++ app/Models/Flight.php
@@ -2,26 +2,31 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Flight extends Model
 {
     use HasFactory;
-    protected $filable = [
-        
-            "date",
-            "departure",
-            "arrival",
-            "image",
-            "airplane_id",
-            "available"
+
+    protected $fillable = [
+        "date",
+        "departure",
+        "arrival",
+        "image",
+        "airplane_id",
+        "available"
     ];
 
-    public function Planes(): BelongsTo
+    public function airplane(): BelongsTo
     {
-        return $this->belongsTo(Planes::class);
+        return $this->belongsTo(planes::class);
+    }
+
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, "flight_user");
     }
 }
--- app/Models/Planes.php
+++ app/Models/Planes.php
@@ -2,19 +2,19 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Planes extends Model
 {
     use HasFactory;
+
     protected $fillable = [
-
-        'name',
-        'places'
+        "name",
+        "places"
     ];
-
+    
     public function flights(): HasMany
     {
         return $this->hasMany(Flight::class);
