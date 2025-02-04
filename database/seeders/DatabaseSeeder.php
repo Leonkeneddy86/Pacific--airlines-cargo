e44ceddf645d0e35c8fd650e30936ead68c97ef3
Leonkeneddy86 <jonathan19.jtv@gmail.com>
2025-02-04 21:32:15 +0100

    fix: commentaries

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,12 +8,11 @@ class DatabaseSeeder extends \Illuminate\Database\Seeder
 {
     public function run(): void
     {
-        // Crea algunos aviones
+    
         $planes = Planes::factory()->count(10)->create();
 
-        // Luego crea algunos vuelos, asegurándote de usar IDs de aviones existentes
         Flight::factory()->count(10)->create([
-            'airplane_id' => $planes->random()->id, // Usa un ID aleatorio de los aviones creados
+            'airplane_id' => $planes->random()->id,
         ]);
     }
 }
