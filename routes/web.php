46ce902a0ba901e6a71e26d5e87758e317540583
Leonkeneddy86 <jonathan19.jtv@gmail.com>
2025-02-10 14:53:53 +0100

    feat: middledwares

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
 use App\Http\Controllers\FlightController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Middleware\Admin;
 
 Route::get('/', function () {
     return view('welcome');
@@ -11,8 +13,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [HomeController::class, 'index'])->middleware('Admin')->name('home');
 
 Route::get("/planes",  [FlightController::class, "index"])->name("planesindex");
-
 Route::get("/show/{id}", [FlightController::class, "show"])->name("planeshow");
-
