02f69a9e6e98bf4cd934bc66545bc793b39e9376
Leonkeneddy86 <jonathan19.jtv@gmail.com>
2025-03-24 19:55:35 +0100

    Feat: Route UserController for view

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Middleware\Admin;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\FlightController;
 use App\Http\Controllers\PlanesController;
@@ -15,3 +16,4 @@ Route::get("/flightsShow/{id}", [FlightController::class, "show"])->name("show")
 Route::get('/planes', [PlanesController::class, 'index'])->middleware("auth", admin::class)->name('planes');
 Route::get('/flightsCreate', [FlightController::class, 'create'])->name('flightsCreate');
 Route::get('/Error', [AdminController::class, 'index'])->name('Error');
+Route::get('index', [UserController::class, 'index'])->name('index');
