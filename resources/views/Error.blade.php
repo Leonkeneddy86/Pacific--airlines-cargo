cb1674c9bc06766793c32c136e479010e2dee1fb
Leonkeneddy86 <jonathan19.jtv@gmail.com>
2025-03-24 19:48:15 +0100

    fix: AdminController route view

--- resources/views/Error.blade.php
+++ resources/views/Error.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-<div role="alert" class="rounded-md border border-gray-300 bg-white p-4 shadow-sm">
+<div role="alert" class="rounded-md border border-red-300 bg-white p-4 shadow-sm">
     <div class="flex items-start gap-4">
       <svg
         xmlns="http://www.w3.org/2000/svg"
@@ -10,38 +10,40 @@
         viewBox="0 0 24 24"
         stroke-width="1.5"
         stroke="currentColor"
-        class="size-6 text-green-600"
+        class="w-6 text-red-600"
       >
         <path
           stroke-linecap="round"
           stroke-linejoin="round"
-          d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+          d="M12 9v2m0 4h.01M12 17a5 5 0 110-10 5 5 0 010 10z"
         />
       </svg>
   
       <div class="flex-1">
-        <strong class="font-medium text-gray-900"> Changes saved </strong>
-  
-        <p class="mt-0.5 text-sm text-gray-700">Your product changes have been saved.</p>
+        <strong class="font-medium text-gray-900">401 Acceso No Autorizado</strong>
+        <p class="mt-0.5 text-sm text-gray-700">
+          No tienes permisos para acceder a este recurso. Por favor, contacta al administrador si crees que se trata de un error.
+        </p>
       </div>
   
       <button
         class="-m-3 rounded-full p-1.5 text-gray-500 transition-colors hover:bg-gray-50 hover:text-gray-700"
         type="button"
-        aria-label="Dismiss alert"
+        aria-label="Cerrar alerta"
       >
-        <span class="sr-only">Dismiss popup</span>
-  
+        <span class="sr-only">Cerrar alerta</span>
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
           stroke-width="1.5"
           stroke="currentColor"
-          class="size-5"
+          class="w-5"
         >
           <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
         </svg>
       </button>
     </div>
-  </div>
+</div>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Middleware\Admin;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\FlightController;
 use App\Http\Controllers\PlanesController;
 
@@ -13,4 +14,4 @@ Route::get("/flights",  [FlightController::class, "index"])->name("flights");
 Route::get("/flightsShow/{id}", [FlightController::class, "show"])->name("show");
 Route::get('/planes', [PlanesController::class, 'index'])->middleware("auth", admin::class)->name('planes');
 Route::get('/flightsCreate', [FlightController::class, 'create'])->name('flightsCreate');
-
+Route::get('/Error', [AdminController::class, 'index'])->name('Error');
